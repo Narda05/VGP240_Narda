a10632b9e1bc3524a477699c1a483d4503b5c0f3
Narda05 <153568349+Narda05@users.noreply.github.com>
2025-01-08 13:13:13 -0800

    Corrections

--- Pix/Pix/MathHelper.h
+++ Pix/Pix/MathHelper.h
@@ -1,29 +1,29 @@
-//#pragma once
-//#include "Vector2.h"
-//#include "Vector3.h"
-//
-//// share equation
-//
-//namespace MathHelper
-//{
-//	bool IsEquial(float s, float b); 
-//
-//
-//	float MagnitudeSquared(const Vector2& v);
-//	float MagnitudeSquared(const Vector3& v);
-//
-//	float Magnitude(const Vector2& v);
-//	float Magnitude(const Vector3& v);
-//
-//	Vector2 Normalize(const Vector2& v);
-//	Vector3 Normalize(const Vector3& v);
-//
-//	//take two vector and return the angle 
-//	float Dot (const Vector2& a, const Vector2& b);
-//	float Dot(const Vector3& a, const Vector3& b);
-//
-//	Vector3 Cross(const Vector3& a, const Vector3& b);
-//
-//
-//
-//}
+#pragma once
+#include "Vector2.h"
+#include "Vector3.h"
+
+// share equation
+
+namespace MathHelper
+{
+	bool IsEquial(float s, float b); 
+
+
+	float MagnitudeSquared(const Vector2& v);
+	float MagnitudeSquared(const Vector3& v);
+
+	float Magnitude(const Vector2& v);
+	float Magnitude(const Vector3& v);
+
+	Vector2 Normalize(const Vector2& v);
+	Vector3 Normalize(const Vector3& v);
+
+	//take two vector and return the angle 
+	float Dot (const Vector2& a, const Vector2& b);
+	float Dot(const Vector3& a, const Vector3& b);
+
+	Vector3 Cross(const Vector3& a, const Vector3& b);
+
+
+
+}
--- Pix/Pix/Vector3.h
+++ Pix/Pix/Vector3.h
@@ -1,27 +1,26 @@
-//#pragma once
-//
-//struct Vector3
-//{
-//	float x, y, z;
-//
-//	Vector3() : x(0), y(0), z(0.0f) {}
-//	Vector3(float s) : x(s), y(s), z(s) {}
-//	Vector3(float x, float y) : x(x), y(y), z(z) {}
-//	Vector3(const Vector3& v) : x(v.x), y(v.y), z(v.z) {}
-//
-//	//operators oveload 
-//	//The simbols those this functions are overloading are the operators using the simbols of maths 
-//	Vector3 operator-() { return Vector3(-x, -y, -z); }
-//	//                only whant the data for that using const 
-//	Vector3 operator+(const Vector3& rhs) { return Vector3(x + rhs.x, y + rhs.y, z + rhs.z); }
-//	Vector3 operator-(const Vector3& rhs) { return Vector3(x - rhs.x, y - rhs.y, z - rhs.z); }
-//	Vector3 operator*(float s) { return Vector3(x * s, y * s, z *s); }
-//	//
-//	Vector3 operator/(float s) { return Vector3(x / s, y / s, z/ s); }
-//
-//	Vector3& operator+=(const Vector3& rhs) { x += rhs.x; y += rhs.y; z += rhs.z  return *this; }
-//	Vector3& operator-=(const Vector3& rhs) { x -= rhs.x; y -= rhs.y; z -= rhs.z return *this; }
-//	Vector3& operator*=(float s) { x *= s; y *= s; z *= s return *this; }
-//	Vector3& operator/=(float s) { x /= s; y /= s; z /= s return *this; }
-//
-//};
+#pragma once
+
+struct Vector3
+{
+	float x, y, z;
+
+	Vector3() : x(0.0f), y(0.0f), z(0.0f) {}
+	Vector3(float s) : x(s), y(s), z(s) {}
+	Vector3(float x, float y, float z) : x(x), y(y), z(z) {}
+	Vector3(const Vector3& v) : x(v.x), y(v.y), z(v.z) {}
+
+	//operators oveload 
+	//The simbols those this functions are overloading are the operators using the simbols of maths 
+	Vector3 operator-() const { return Vector3(-x, -y, -z); }
+	//                only whant the data for that using const 
+	Vector3 operator+(const Vector3& rhs) const { return Vector3(x + rhs.x, y + rhs.y, z + rhs.z); }
+	Vector3 operator-(const Vector3& rhs) const { return Vector3(x - rhs.x, y - rhs.y, z - rhs.z); }
+	Vector3 operator*(float s) const { return Vector3(x * s, y * s, z *s); }
+	Vector3 operator/(float s) const { return Vector3(x / s, y / s, z/ s); }
+
+	Vector3& operator+=(const Vector3& rhs) { x += rhs.x; y += rhs.y; z += rhs.z;  return *this; }
+	Vector3& operator-=(const Vector3& rhs) { x -= rhs.x; y -= rhs.y; z -= rhs.z; return *this; }
+	Vector3& operator*=(float s) { x *= s; y *= s; z *= s; return *this; }
+	Vector3& operator/=(float s) { x /= s; y /= s; z /= s; return *this; }
+
+};
